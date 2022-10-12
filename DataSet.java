8dfecc4b8bb934385819fd557bcce5d094d28727
Most-Rima-Khatun <113694175+Most-Rima-Khatun@users.noreply.github.com>
2022-10-12 23:39:37 +0600

    Add files via upload

---
+++ DataSet.java
@@ -0,0 +1,40 @@
+public class DataSet
+    {
+
+        public DataSet(int maximumNumberOfValues)
+        {
+
+        }
+
+        public void add(double value)
+        {
+
+        }
+
+
+        public double getSum()
+        {
+             double sum = getSum();
+        }
+
+
+        public double getAverage()
+        {
+          getAverage();
+        }
+
+
+        public double getMaximum()
+        {
+            double maximum = getMaximum();
+        }
+
+
+        public double getMinimum()
+        {
+             getMinimum();
+        }
+    }
+
+
+}
