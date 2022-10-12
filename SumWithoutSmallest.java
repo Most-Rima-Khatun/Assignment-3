de1884bfcc8cf74572db3018d4bcffdd77696c97
Most-Rima-Khatun <113694175+Most-Rima-Khatun@users.noreply.github.com>
2022-10-12 17:40:21 +0600

    Add files via upload

---
+++ SumWithoutSmallest.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SumWithoutSmallest {
+    public static void main(String[] args) {
+        Scanner number =new Scanner(System.in);
+        int sum = 0;
+        final int TOTALNUMBERS = 10;
+        int value[]=new int[TOTALNUMBERS];
+
+        for(int i=0;i<=10;i++)
+        {
+
+            value[i]=number.nextInt() ;
+
+        }
+
+
+
+
+    }
+}
